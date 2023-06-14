2821912c832eb20009ba68645f6e50c83ae0f6d5
Erica Belzer <belzer49@gmail.com>
2023-06-14 09:48:28 -0500

    creates myfile.md

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Links from "./Links"
+import user from "../data/user";
+import { render } from "@testing-library/react";
 
-function About() {
+
+function About(props) {
+  let paragraph;
+  if(props.bio){
+    paragraph =<p>{props.bio}</p>
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {paragraph}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
